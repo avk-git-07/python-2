55140cc35644c3271d78b301a672b793ceb9c154
avk-git-07 <asurivamshi@gmail.com>
2026-04-02 13:07:06 +0530

    commit of test-1.py

--- test-1.py
+++ test-1.py
@@ -1,4 +1,4 @@
 n1 = 10
 n2 = 5
 res = n1 // n2
-print("Integer Division of Number:", n1,"and", "Number:", n2, "is ", res)
+print("The quotient when Number:", n1,"divided with", "Number:", n2, "is ", res)
