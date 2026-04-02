8a237b57aaec2cbdea0c999145b4fb929a22862c
avk-git-07 <asurivamshi@gmail.com>
2026-04-02 12:38:26 +0530

    commit of test-1.py

--- test-1.py
+++ test-1.py
@@ -1,5 +1,4 @@
-arn = ("arn:aws:iam::123456789012:user/john", 1, 2, "arn:aws:iam::123456789012:user/peter")
-for user in arn:
-    print(str(user).split("/")[-1])
+arn = "vamshi"
+print(arn.upper())
 
     
