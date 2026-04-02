4097aa6890d4a198b27a8d59d1a621370607438e
avk-git-07 <asurivamshi@gmail.com>
2026-04-02 12:32:11 +0530

    commit of test-1.py

--- test-1.py
+++ test-1.py
@@ -1,6 +1,5 @@
 arn = ("arn:aws:iam::123456789012:user/john", 1, 2, "arn:aws:iam::123456789012:user/peter")
 for user in arn:
-    if isinstance(user, str):
-        print(user.split("/")[1])
+    print(str(user).split("/")[1])
 
     
